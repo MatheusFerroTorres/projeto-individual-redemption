8321d30a925a57f9d5855c78bff99430ce5f5775
MatheusFerroTorres <matheus.torres@sptech.school>
2024-06-06 01:32:11 -0300

    Criando validacao para cadastro na API

--- public/CADASTRO/script-cadastro.js
+++ public/CADASTRO/script-cadastro.js
@@ -10,7 +10,7 @@ function cadastrar() {
     var aceito = 0;
 
     /* Validação do nome */
-    if(nome.length < 3) {
+    if (nome.length < 3) {
         ipt_nome.style.borderBottom = '1px solid #FF0000';
         erro_nome.style.display = 'block'
     } else {
@@ -19,72 +19,78 @@ function cadastrar() {
         aceito++
     }
 
-   /* Validação da senha */
-if (senha.length < 6 && (senha.indexOf('&') == -1 || senha.indexOf('@') == -1 || senha.indexOf('#') == -1 || senha.indexOf('$') == -1 || senha.indexOf('%') == -1)) {
-    ipt_senha.style.borderBottom = '1px solid #FF0000';
-    erro_senha1.style.display = 'block';
-} else {
-    ipt_senha.style.borderBottom = '1px solid #FFFFFF';
-    erro_senha1.style.display = 'none';
-    aceito++;
-}
-
-   /* Validação da confirmação da senha */
-   if(confirmacaoSenha != senha) {
-    ipt_confirmar.style.borderBottom = '1px solid #FF0000';
-    erro_senha2.style.display = 'block'
-} else {
-    ipt_confirmar.style.borderBottom = '1px solid #FFFFFF';
-    erro_senha2.style.display = 'none'
-    aceito++
-}
-
-/* Validação do e-mail */
-if(email.indexOf('@') == -1 || email.indexOf('.') == -1) {
-    ipt_email.style.borderBottom = '1px solid #FF0000';
-    erro_email.style.display = 'block'
-} else {
-    ipt_email.style.borderBottom = '1px solid #FFFFFF';
-    erro_email.style.display = 'none'
-    aceito++
-}
-
-/* Validação do telefone */
-if(telefone.length != 11) {
-    ipt_telefone.style.borderBottom = '1px solid #FF0000';
-    erro_telefone.style.display = 'block'
-} else {
-    ipt_telefone.style.borderBottom = '1px solid #FFFFFF';
-    erro_telefone.style.display = 'none'
-    aceito++
-}
-
-/* Validação da idade */
-if (idade <18){
-    ipt_idade.style.borderBottom = '1px solid #FF0000';
-    erro_idade.style.display = 'block'
-} else{
-    ipt_idade.style.borderBottom = '1px solid #FFFFFF';
-    erro_idade.style.display = 'none'
-    aceito++
-}
-
-fetch('/usuarios/cadastrar', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        nomeServer: nome,
-        emailServer: email,
-        senhaServer: senha,
-        telefoneServer: telefone
-    })
-}) .then(res =>{
-    if (res.ok){
-        window.open('/LOGIN/LOGIN.html', '_self')
-    } else{
-        res.json().then(response => alert(response))
+    /* Validação da senha */
+    if (senha.length < 6 && (senha.indexOf('&') == -1 || senha.indexOf('@') == -1 || senha.indexOf('#') == -1 || senha.indexOf('$') == -1 || senha.indexOf('%') == -1)) {
+        ipt_senha.style.borderBottom = '1px solid #FF0000';
+        erro_senha1.style.display = 'block';
+    } else {
+        ipt_senha.style.borderBottom = '1px solid #FFFFFF';
+        erro_senha1.style.display = 'none';
+        aceito++;
     }
-})
+
+    /* Validação da confirmação da senha */
+    if (confirmacaoSenha != senha) {
+        ipt_confirmar.style.borderBottom = '1px solid #FF0000';
+        erro_senha2.style.display = 'block'
+    } else {
+        ipt_confirmar.style.borderBottom = '1px solid #FFFFFF';
+        erro_senha2.style.display = 'none'
+        aceito++
+    }
+
+    /* Validação do e-mail */
+    if (email.indexOf('@') == -1 || email.indexOf('.') == -1) {
+        ipt_email.style.borderBottom = '1px solid #FF0000';
+        erro_email.style.display = 'block'
+    } else {
+        ipt_email.style.borderBottom = '1px solid #FFFFFF';
+        erro_email.style.display = 'none'
+        aceito++
+    }
+
+    /* Validação do telefone */
+    if (telefone.length != 11) {
+        ipt_telefone.style.borderBottom = '1px solid #FF0000';
+        erro_telefone.style.display = 'block'
+    } else {
+        ipt_telefone.style.borderBottom = '1px solid #FFFFFF';
+        erro_telefone.style.display = 'none'
+        aceito++
+    }
+
+    /* Validação da idade */
+    if (idade < 18) {
+        ipt_idade.style.borderBottom = '1px solid #FF0000';
+        erro_idade.style.display = 'block'
+    } else {
+        ipt_idade.style.borderBottom = '1px solid #FFFFFF';
+        erro_idade.style.display = 'none'
+        aceito++
+    }
+
+    // Parte responsável por direcionar os inputs para o banco usando a API
+    // Validando se todos os campos passaram
+    if (aceito == 6) {
+        fetch('/usuarios/cadastrar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                nomeServer: nome,
+                emailServer: email,
+                senhaServer: senha,
+                telefoneServer: telefone
+            })
+        }).then(res => {
+            if (res.ok) {
+                window.open('/LOGIN/LOGIN.html', '_self')
+            } else {
+                res.json().then(response => alert(response))
+            }
+        })
+    } else {
+        alert(`Falha ao cadastrar!`)
+    }
 }
